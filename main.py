90ad36e569fe3c64a45a6fe39d337238ab508c75
= <=>
2020-01-17 04:02:55 +0100

    'calculadora'

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter import ttk
-from romannumber import *
 
 from calculator import *
 from calculator import WIDTHBTN, HEIGHTBTN
--- romannumber.py
+++ romannumber.py
@@ -2,15 +2,15 @@ class RomanNumber():
     value = 0
     __romanValue = ''
 
-    __valores = {'I': 1, 'V': 5, 'X':10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    __valores_5 = {'V': 5,'L': 50,'D': 500}
+    __valores = {'I':1, 'V': 5, 'X':10, 'L': 50, 'C':100, 'D': 500, 'M': 1000}
+    __valores5 = { 'V': 5,  'L': 50,  'D': 500 } 
     __simbolosOrdenados = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
 
     __rangos = {
-        0: {1: 'I', 5: 'V', 'next': 'X'},
-        1: {1: 'X', 5: 'L', 'next': 'C'},
-        2: {1: 'C', 5: 'D', 'next': 'M'},
-        3: {1: 'M', 'next': 'X'},
+        0: {1: 'I', 5 : 'V', 'next': 'X'},
+        1: {1: 'X', 5 : 'L', 'next': 'C'},
+        2: {1: 'C', 5 : 'D', 'next': 'M'},
+        3: {1: 'M', 'next': ''}
     }
 
     def __init__(self, value=None):
@@ -23,6 +23,10 @@ class RomanNumber():
         else:
             raise TypeError('Argumento de RomanNumber ha de ser un entero o una cadena')
 
+    def cadenaAromano(self, cadena):
+        self.__romanValue = cadena
+        self.value = self.__romano_a_arabigo()
+
     def __invertir(self,cad):
         return cad[::-1] #para darle la vuelta a la cifra
         
@@ -30,8 +34,8 @@ class RomanNumber():
         cad = str(self.value)#esto ya es self.value
         dac = self.__invertir(cad)
         grupos = []
-        rango = 0
 
+        rango = 0
         for i in range(0, len(cad), 3): #desde 0 hasta len, de 3 en 3
             grupos.append([rango, int(self.__invertir(dac[i:i+3]))]) # hacemos otro invertir para poner los numeros al derecho
             rango += 1 #el rango son los parentesis, al leer el numero desde atras, vamos aumentando en 1 en cada grupo
@@ -100,12 +104,16 @@ class RomanNumber():
             grupo = grupoParentesis[ix]  # metemos el primero en grupo, ya que ix es 0
             numP = self.__numParentesis(grupo)  # metemos en numP, el numero de parentesis de ese grupo
             if numP > 0:                 # si el numero de parentesis es mayor que 0, entra
+                if ix+numP >= len(grupoParentesis):
+                    raise ValueError('Número de paréntesis incorrecto - Faltan cierres')
                 for j in range(ix+1, ix+numP):     # para j, en el rango 1(en el primer caso) y el numero de parentesis
                     if grupoParentesis[j] != '':   #asi verifica que hay parentesis despues del numero, entre grupo y grupo
                         raise ValueError('Simbolos entre parentesis de cierre') #explota o Falla
                 ix += numP - 1 # restamos 1 al numero de parentesis porque el numero de espacios entre ellos, es uno menos
                             # y asi tenemos el ix listo para que pase al siguiente grupo
-            
+            else:
+                if len(grupoParentesis)-ix > 1:
+                    raise ValueError('Número de paréntesis incorrecto - Sobran cierres')
             if len(grupo[numP:]) > 0: #si lo que va despues de los parentesis es mayor que 0
                 res.append([numP, grupo[numP:]]) # metes el numero de parentesis y lo que va despues de los parentesis en el grupo
             ix += 1
@@ -131,14 +139,14 @@ class RomanNumber():
                     numRepes = 1
                 elif self.__valores[ultimoCaracter] == self.__valores[letra]: #si hay dos numeros iguales seguidos
                     numRepes += 1
-                    if letra in self.__valores_5 and ultimoCaracter in self.__valores_5: #solo deberiamos comprobar uno, porque en el elif ya dice que son iguales
+                    if letra in self.__valores and ultimoCaracter in self.__valores: #solo deberiamos comprobar uno, porque en el elif ya dice que son iguales
                         raise ValueError('Mas de un valor de 5 repetido')
                     if numRepes > 3:
                         raise ValueError('mas de 3 repeticiones')
                 elif self.__valores[ultimoCaracter] < self.__valores[letra]: #cuando hay 2 numeros menores delante de uno mayor
                     if numRepes > 1: #no permite repeticiones en las restas
                         raise ValueError('No se ademiten repeticiones en restas')
-                    if ultimoCaracter in self.__valores_5: #no permite restas de valores de 5 (5, 50, 500)
+                    if ultimoCaracter in self.__valores: #no permite restas de valores de 5 (5, 50, 500)
                         raise ValueError('No se pueden restar valores de 5')
                     distancia = self.__simbolosOrdenados.index(letra) - self.__simbolosOrdenados.index(ultimoCaracter) #No permite que se resten unidades de mas de un orden
                     if distancia > 2:
@@ -146,7 +154,7 @@ class RomanNumber():
                     numArabigo -= self.__valores[ultimoCaracter] * 2
                     numRepes = 1
 
-            else: #si el simbolo romano no está permitido, devolvemos error (0)
+            else:  #si el simbolo romano no está permitido, devolvemos error (0)
                 raise ValueError('Simbolo incorrecto') 
 
             ultimoCaracter = letra
@@ -157,19 +165,61 @@ class RomanNumber():
         numArabigoTotal = 0
         res = self.__contarParentesis()
 
-        for grupo in res:  # recorres los grupos
-            romano = grupo[1] #la posicion 1 de el grupo en el que estas, es decir el numero en si
-            factor = pow(10, 3 * grupo[0]) #elevas a 10, el 3 multiplicado por el numero de parentesis
-            numArabigoTotal += self.__romano_individual(romano) * factor 
+        for elemento in res:
+            romano = elemento[1]
+            factor = pow(10, 3 * elemento[0])
+
+            numArabigoTotal += self.__romano_individual(romano) * factor
 
         return numArabigoTotal
 
     def __str__(self):
-        return (self.__romanValue)
+        return "{}".format(self.__romanValue)
+
+    def __int__(self):
+        return self.value
 
     def __repr__(self):
         return self.__romanValue
+
+    def __add__(self, value): #self es el numero instanciado, el primero, y value es lo que queremos sumarle
+        resultado = int(value) + self.value  #me haces el entero de value y a self le aplicas value para que sea entero
+        resultado = RomanNumber(resultado)
+        return resultado
+
+    def __radd__(self, value): # con esta funcion espejo, si lo de arriba no se puede hacer, esto le da la vuelta los valores, y el value de arriba ahora es el self
+        return self.__add__(value)
+
+    def __sub__(self, value):
+        resultado = max(0, self.value - int(value))
+        resultado = RomanNumber(resultado)
+        return resultado
+
+
+    def __rsub__(self, value):
+        return self.__sub__(value)
+
+    def __mul__(self, value):
+        resultado = int(value) * self.value
+        resultado = RomanNumber(resultado)
+        return resultado
+
+    def __rmul__(self, value):
+        return self.__mul__(value)
+
+    def __truediv__(self, value):
+        resultado = int(value) // self.value
+        resultado = RomanNumber(resultado)
+        return resultado
+
+    def __rtruediv__(self, value):
+        return self.__rtruediv__(value)
+
+    def __div__(self, value):
+        return self.__truediv__(value)
     
-    def __add__(self, value):
-        #esto no funcionará
-        return RomanNumber(value) + self.value
+    def __rdiv__(self, value):
+        return self.__div__(value)
+
+    def __lt__(self, value):
+        return int(self) < int(value)
